6ffa8436936642dc8e35a5f47c09a97dee205d08
DUT-Team66 <115085544+DUT-Team66@users.noreply.github.com>
2022-10-25 10:40:33 +0700

    update toán tử gán

--- StaffManager.cpp
+++ StaffManager.cpp
@@ -247,3 +247,14 @@ void StaffManager::Menu() {
         std::system("cls");
     }
 }
+const StaffManager& StaffManager::operator=(const StaffManager& v )
+{
+    if (this != &v) {
+        this->n=v.n;
+        delete[] this->p;
+        this->p = new int[this->n];
+        for (int i=0;i<this->n;i++)
+            (*this)[i] = *(v.p+i); 
+    }
+    return *this;
+}
